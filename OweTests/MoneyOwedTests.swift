6d409b1deeb31ed3d8266fbdd9c98f580cc725f6
Kyle McAlpine <k@kylejm.io>
2016-06-10 20:46:41 +1000

    Added a test

--- OweTests/MoneyOwedTests.swift
+++ OweTests/MoneyOwedTests.swift
@@ -29,6 +29,18 @@ class OweTests: XCTestCase {
     }
     
     func testCalculationsWith4People() {
+        let person1 = Person(name: "1")
+        let person2 = Person(name: "2")
+        let person3 = Person(name: "3")
+        let person4 = Person(name: "4")
         
+        _ = Expense(amount: 400, desc: "test", title: "test", person: person1)
+        
+        do {
+            try MoneyOwed.calculateForPeople([person1, person2, person3, person4])
+            let owed = try MoneyOwed.forPerson(person1)
+            XCTAssertEqual(owed.count, 3)
+            XCTAssertEqual(owed.first?.amount, -100)
+        } catch { XCTFail("Something threw") }
     }
 }
